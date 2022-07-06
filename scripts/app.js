43c92eb0bad240be766eb030df4b81a52e3a042e
DragleGame <108300247+DragleGame@users.noreply.github.com>
2022-07-06 06:42:25 -0300

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -750,9 +750,11 @@ function autocomplete(inp, arr) {
         b.innerHTML = arr[i].substr(0, val.length);
         b.innerHTML += arr[i].substr(val.length);
         /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        b.innerHTML +=
+          "<input type='hidden' value='" + arr[i].replace(/'/g, "&#39;") + "'>";
         /*execute a function when someone clicks on the item value (DIV element):*/
         b.addEventListener("click", function (e) {
+          alert(this.getElementsByTagName("input")[0].value);
           /*insert the value for the autocomplete text field:*/
           inp.value = this.getElementsByTagName("input")[0].value;
           /*close the list of autocompleted values,
